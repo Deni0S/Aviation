176907fc62b13684274ee68192a106cd04f836d0
Denis <DgamFX@mail.ru>
2019-08-18 21:26:16 +0300

    Initial Commit

---
+++ Aviation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aviation
+//
+//  Created by Денис Баринов on 18/08/2019.
+//  Copyright © 2019 Денис Баринов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
