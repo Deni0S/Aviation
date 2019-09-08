166bbb262a8ac8946078cb29a0ddeb4e513f9488
Denis <DgamFX@mail.ru>
2019-09-08 12:30:34 +0300

    Добавлен дизайн и исправлена логика работы демо режима скорости
    Добавлена отрисованная линейка скорости в трех размерах. Настроен внешний вид Picker View. Действия индикаторов скорости перенесены в один метод и изменена их логика.

--- Aviation/Controllers/PFDViewController.swift
+++ Aviation/Controllers/PFDViewController.swift
@@ -13,74 +13,65 @@ class PFDViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     @IBOutlet weak var speedRuler: UIImageView!
     @IBOutlet weak var speedRulerUIView: UIView!
     @IBOutlet weak var skyline: UIImageView!
-    @IBOutlet weak var SpeedIndicatorPicker: UIPickerView!
+    @IBOutlet weak var speedIndicatorPicker: UIPickerView! {
+        didSet {
+            speedIndicatorPicker.delegate = self
+            speedIndicatorPicker.dataSource = self
+        }
+    }
     
-    var timeAnimation: Double = 10
+    var timeInterval: Double = 0.1
+    var timeAnimation: Double = 21
     var centerOfActionView = CGPoint()
-    var speed: [String] = ["0","9","8","7","6","5","4","3","2","1","0","9"]
-    //["0","1", "2","3","4","5","6","7","8","9","0"]
+    var speed: Int = 0
+    var speedPicker: [String] = ["0","9","8","7","6","5","4","3","2","1","0","9"]
+    var centerX: Int = 0
+    var centerY: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        SpeedIndicatorPicker.delegate = self
-        SpeedIndicatorPicker.dataSource = self
-        speedRulerAction()
-        speedIndicator()
-        SpeedIndicatorPickerView()
+        self.centerX = Int(self.speedRulerUIView.center.x)
+        self.centerY = Int(self.speedRulerUIView.center.y)
+        debugPrint(centerX,centerY)
+        speedIndicatorAction()
+    }
+    
+    // Действия индикторов скорости
+    func speedIndicatorAction() {
+        DispatchQueue.global().async {
+            Timer.scheduledTimer(withTimeInterval: self.timeInterval, repeats: true) { timer in
+                DispatchQueue.main.async {
+                    self.speed += 1
+                    self.speedIndicatorSmall.text = "\(self.speed/10)"
+                    self.speedIndicatorPicker.selectRow(10-self.speed%10, inComponent: 0, animated: false)
+                    UIView.animate(withDuration: self.timeInterval,
+                                   delay: 0,
+                                   options: UIView.AnimationOptions.curveLinear,
+                                   animations: {
+                                    self.speedRulerUIView.center = CGPoint(x: self.centerX, y: self.centerY+10*self.speed)
+                    })
+                }
+            }
+            RunLoop.current.run(until: Date()+self.timeAnimation)
+        }
+        DispatchQueue.global().asyncAfter(deadline: .now()+timeAnimation) {
+            Timer.scheduledTimer(withTimeInterval: self.timeInterval, repeats: true) { timer in
+                DispatchQueue.main.async {
+                    self.speed -= 1
+                    self.speedIndicatorSmall.text = "\(self.speed/10)"
+                    self.speedIndicatorPicker.selectRow(10-self.speed%10, inComponent: 0, animated: false)
+                    UIView.animate(withDuration: self.timeInterval,
+                                   delay: 0,
+                                   options: UIView.AnimationOptions.curveLinear,
+                                   animations: {
+                                    self.speedRulerUIView.center = CGPoint(x: self.centerX, y: self.centerY+10*self.speed)
+                    })
+                }
+            }
+            RunLoop.current.run(until: Date()+self.timeAnimation)
+        }
     }
 
-    // Движение линейки спидометра
-    func speedRulerAction() {
-        centerOfActionView = self.speedRulerUIView.center
-        UIView.animate(withDuration: TimeInterval(self.timeAnimation),
-                       delay: 0,
-                       options: UIView.AnimationOptions.curveLinear,
-                       animations: {
-                        self.speedRulerUIView.center = CGPoint(x: 128, y: 500)
-        }) { (true) in
-            UIView.animate(withDuration: TimeInterval(self.timeAnimation), animations: {
-                self.speedRulerUIView.center = self.centerOfActionView
-            })
-        }
-    }
-    
-    // Движение индиктора скорости
-    func speedIndicator() {
-        var speeds = 0
-        DispatchQueue.global().async {
-            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
-//                for speed in stride(from: 0,through: 100, by: 10) {
-//                autoreleasepool{
-                    DispatchQueue.main.async {
-                        self.speedIndicatorSmall.text = "\(speeds)"
-                        self.SpeedIndicatorPicker.selectRow(10-speeds%10, inComponent: 0, animated: true)
-                        speeds += 1
-                    }
-//                }
-//                sleep(1)
-            }
-            RunLoop.current.run(until: Date()+11)
-        }
-        DispatchQueue.global().asyncAfter(deadline: .now()+10) {
-            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
-//                for speed in stride(from: -100,through: 30, by: 10) {
-//                autoreleasepool{
-                    DispatchQueue.main.async {
-                        speeds -= 1
-                        self.speedIndicatorSmall.text = "\(speeds)"
-                        self.SpeedIndicatorPicker.selectRow(10-speeds%10, inComponent: 0, animated: true)
-                    }
-//                }
-//                sleep(1)
-            }
-            RunLoop.current.run(until: Date()+11)
-        }
-    }
-    
-    // Движение индиктора скорости в виде PickerView
-    func SpeedIndicatorPickerView() {
-    }
-    
     // Количество компонентов в PickerView
     func numberOfComponents(in SpeedIndicatorPicker: UIPickerView) -> Int {
         return 1
@@ -88,23 +79,23 @@ class PFDViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     
     // Количество значений в PickerView
     func pickerView(_ SpeedIndicatorPicker: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return speed.count
+        return speedPicker.count
     }
     
     // Значение очередной строки в PickerView
     func pickerView(_ SpeedIndicatorPicker: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return speed[row]
+        return speedPicker[row]
     }
     
-    // Сменим цвет текста в PickerView
-//    func pickerView(_ SpeedIndicatorPicker: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
-//        let string = speed[indexPath.row]
-//        return NSAttributedString(string: string, attributes: [NSAttributedString.key.foregroundColor: UIColor.white])
-//    }
+    // Внешний вид индикатора скорости PickerView
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        return NSAttributedString(string: self.speedPicker[row], attributes:  [NSAttributedString.Key.foregroundColor:UIColor.white, NSAttributedString.Key.strokeWidth:9,  NSAttributedString.Key.kern:50,  NSAttributedString.Key.underlineStyle:false,  NSAttributedString.Key.strokeColor:UIColor.white
+            ])
+    }
     
     // Движение линии горизонта
     func skylineAction() {
-        
     }
     
 }
+
